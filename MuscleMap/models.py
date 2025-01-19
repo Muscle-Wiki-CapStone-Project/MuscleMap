4e6fd71e3a22c2403695ab161ccf183a87a54d0b
Patrick Dacius <patrickdacius27@gmail.com>
2025-01-19 15:43:03 -0500

    Exercise model created"

--- MuscleMap/models.py
+++ MuscleMap/models.py
@@ -9,7 +9,7 @@ class User(db.Model, UserMixin):
     #user model will store users and their info
     __tablename__ = 'users'
 
-    id = db.Column(db.integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.column(db.string(100), nullable=False, unique=True)
     gender = db.column(db.string(20), nullable=True)
     password_hash = db.column(db.string(128), nullable=False)
@@ -29,7 +29,7 @@ class MuscleGroup(db.Model):
     __tablename__ = 'muscleGroup'
 
 
-    muscle_group_id = db.Column(db.integer, primary_key=True)
+    muscle_group_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.string(100), nullable=False)
     description = db.Column(db.string(255), nullable=True)
 
@@ -37,5 +37,20 @@ class MuscleGroup(db.Model):
 
     exercises = db.relationship('Exercise', backref='muscle_group', lazy=True)
 
+    class Exercise(db.Model):
+        #represents the exercise
+        __tablename__ = 'exercise'
+
+        exercise_id = db.Column(db.Integer, primary_key=True)
+        name = db.Column(db.string(100), nullable=False)
+        description = db.Column(db.string(255), nullable=True)
+        video_url = db.Column(db.string(255), nullable=True)
+        required_equipment = db.Column(db.string(255), nullable=True)
+        muscle_group_id = db.Column(db.Integer, db.ForeignKey('muscleGroup.muscle_group_id') ,nullable=False)
+
+        #Relationships
+
+        favorites = db.relationship('UsersFavorites', backref='exercise', lazy=True)
+        workout_plans = db.relationship('WorkoutPlanDetails', backref='exercise', lazy=True)
 
     
