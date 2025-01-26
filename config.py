d5717a9e0472f21ba3dbdfa2443193ef0c4eb8d9
Patrick Dacius <patrickdacius27@gmail.com>
2025-01-25 20:29:38 -0500

    update

--- config.py
+++
@@ -1,9 +0,0 @@
-import os 
-
-class Config:
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.urandom(24) # used for sessions and cookies (will generate 24 random byte to protect the privacy)
-   
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///musclemap.db'  # Replace with your preferred DB URI
-
-    
